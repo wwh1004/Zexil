07303b3f61078119673d369d93d3b2368f8f7c85
文煌 <wwh1004@hotmail.com>
2020-09-06 00:12:39 +0800

    update JitHelpers

--- Zexil.DotNet.Emulation/JitHelpers.cs
+++ Zexil.DotNet.Emulation/JitHelpers.cs
@@ -3,6 +3,8 @@ using System.Runtime.CompilerServices;
 
 namespace Zexil.DotNet.Emulation {
 	internal static class JitHelpers {
+		private const uint BIT_SBLK_GC_RESERVE = 0x20000000;
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static ref byte GetRawData(this object obj) {
 			return ref Unsafe.As<RawData>(obj).Data;
@@ -11,6 +13,12 @@ namespace Zexil.DotNet.Emulation {
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static ref byte GetRawSzArrayData(this Array array) {
 			return ref Unsafe.As<RawSzArrayData>(array).Data;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void SetGCBit(object obj) {
+			ref uint m_SyncBlock = ref Unsafe.As<byte, uint>(ref Unsafe.Subtract(ref GetRawData(obj), IntPtr.Size + sizeof(uint)));
+			m_SyncBlock |= BIT_SBLK_GC_RESERVE;
 		}
 
 		private sealed class RawData {
