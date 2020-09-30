439d798b5ed0c090134cfb4ab7f05a20e614af4d
文煌 <wwh1004@hotmail.com>
2020-09-30 15:18:59 +0800

    implement default InterpretFromStub

--- Zexil.DotNet.Emulation/Emit/Interpreter.cs
+++ Zexil.DotNet.Emulation/Emit/Interpreter.cs
@@ -10,6 +10,7 @@ namespace Zexil.DotNet.Emulation.Emit {
 	/// </summary>
 	public sealed partial class Interpreter : IInterpreter, IDisposable {
 		private readonly InterpreterContext _context;
+		private Func<ModuleDesc, ModuleDef> _resolveModuleDef;
 		private InterpretFromStubHandler _interpretFromStubUser;
 		private bool _isDisposed;
 
@@ -17,6 +18,14 @@ namespace Zexil.DotNet.Emulation.Emit {
 		/// Interpreter context
 		/// </summary>
 		public InterpreterContext Context => _context;
+
+		/// <summary>
+		/// Resolves a <see cref="ModuleDef"/> instance by <see cref="ModuleDesc"/>
+		/// </summary>
+		public Func<ModuleDesc, ModuleDef> ResolveModuleDef {
+			get => _resolveModuleDef;
+			set => _resolveModuleDef = value;
+		}
 
 		/// <inheritdoc />
 		public InterpretFromStubHandler InterpretFromStubUser {
@@ -86,6 +95,31 @@ namespace Zexil.DotNet.Emulation.Emit {
 		}
 
 		/// <summary>
+		/// Create method context with specified <see cref="MethodDesc"/>
+		/// </summary>
+		/// <param name="moduleDef"></param>
+		/// <param name="method"></param>
+		/// <param name="methodDef"></param>
+		/// <param name="arguments"></param>
+		/// <returns></returns>
+		public InterpreterMethodContext CreateMethodContext(ModuleDef moduleDef, MethodDesc method, MethodDef methodDef, params nint[] arguments) {
+			if (moduleDef is null)
+				throw new ArgumentNullException(nameof(moduleDef));
+			if (method is null)
+				throw new ArgumentNullException(nameof(method));
+			if (methodDef is null)
+				throw new ArgumentNullException(nameof(methodDef));
+			if (methodDef.HasGenericParameters || methodDef.DeclaringType.HasGenericParameters)
+				throw new NotSupportedException($"Creating method context by {nameof(MethodDef)} does NOT support generic method or method in generic type.");
+			if (arguments is null)
+				throw new ArgumentNullException(nameof(arguments));
+
+			var methodContext = _context.AcquireMethodContext(method, moduleDef);
+			methodContext.ResolveDynamicContext(arguments);
+			return methodContext;
+		}
+
+		/// <summary>
 		/// Interprets a CIL instruction with specified method context
 		/// </summary>
 		/// <param name="instruction"></param>
@@ -101,7 +135,42 @@ namespace Zexil.DotNet.Emulation.Emit {
 
 		/// <inheritdoc />
 		public void InterpretFromStub(MethodDesc method, nint[] arguments) {
-			throw new NotImplementedException();
+			var resolveModuleDef = _resolveModuleDef;
+			if (resolveModuleDef is null)
+				throw new InvalidOperationException($"{nameof(ResolveModuleDef)} is null");
+
+			var moduleDef = resolveModuleDef(method.Module);
+			if (moduleDef is null)
+				throw new InvalidOperationException($"Resolving {nameof(ModuleDef)} fails");
+
+			var methodDef = (MethodDef)moduleDef.ResolveToken(method.MetadataToken);
+			var instructions = methodDef.Body.Instructions;
+			using var methodContext = CreateMethodContext(moduleDef, method, methodDef, arguments);
+			for (int i = 0; i < instructions.Count; i++) {
+				InterpretImpl(instructions[i], methodContext);
+				if (!(methodContext.NextILOffset is uint nextILOffset))
+					continue;
+
+				int nextIndex = -1;
+				uint currentILOffset = instructions[i].Offset;
+				int lo = 0;
+				int hi = 0 + instructions.Count - 1;
+				while (lo <= hi) {
+					int j = lo + ((hi - lo) >> 1);
+					int order = (int)instructions[j].Offset - (int)currentILOffset;
+					if (order == 0) {
+						nextIndex = j;
+						break;
+					}
+
+					if (order < 0)
+						lo = j + 1;
+					else
+						hi = j - 1;
+				}
+				if (nextIndex == -1)
+					throw new InvalidOperationException("Offsets of instructions are incorrect or branch target is not in instruction list");
+			}
 		}
 
 		/// <inheritdoc />
